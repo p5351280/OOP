e0b5133c93c8206dc143ca96095c2b73a858bf1c
p5351280 <p5351280@gmail.com>
2017-05-02 02:30:57 +0800

    expect cin cout

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -12,7 +12,7 @@ class Rational{
 		void normalize();
 		friend ostream& operator <<(ostream& outs, const Rational& r);
 		friend istream& operator >>(istream& in, Rational& r);
-        //non member function : +, -(subtraction), -(subtraction)
+		//non member function : +, -(subtraction), -(subtraction)
 		friend Rational operator *(const Rational& r1, const Rational& r2);
 		friend Rational operator /(const Rational& r1, const Rational& r2);
 		friend bool operator ==(const Rational& r1, const Rational& r2);
@@ -64,92 +64,92 @@ void Rational::normalize(){
 	int gcd=GCD(abs(numerator), abs(denominator));
 	numerator /= gcd;
 	denominator /= gcd;
-    if(denominator < 0){
-        numerator = -numerator;
-        denominator = -denominator;
-    }
+	if(denominator < 0){
+		numerator = -numerator;
+		denominator = -denominator;
+	}
 }
 
 //friend
 ostream& operator <<(ostream& outs, const Rational& r){
-    
+
 }
 
 //friend
 istream& operator >>(istream& in, Rational& r){
-    
+
 }
 
 //nonmember
 Rational operator +(const Rational& r1, const Rational& r2){
-    int numerator = r1.getNumerator()*r2.getDenominator() + r2.getNumerator()*r1.getDenominator();
-    int denominator = r1.getDenominator() * r2.getDenominator();
-    return Rational(numerator, denominator);
+	int numerator = r1.getNumerator()*r2.getDenominator() + r2.getNumerator()*r1.getDenominator();
+	int denominator = r1.getDenominator() * r2.getDenominator();
+	return Rational(numerator, denominator);
 }
 
 //nonmember
 Rational operator -(const Rational& r1, const Rational& r2){
-    int numerator = r1.getNumerator()*r2.getDenominator() - r2.getNumerator()*r1.getDenominator();
-    int denominator = r1.getDenominator() * r2.getDenominator();
-    return Rational(numerator, denominator);
+	int numerator = r1.getNumerator()*r2.getDenominator() - r2.getNumerator()*r1.getDenominator();
+	int denominator = r1.getDenominator() * r2.getDenominator();
+	return Rational(numerator, denominator);
 }
 
 //nonmember
 Rational operator -(const Rational& r){
-    int numerator = -(r.getNumerator());
-    int denominator = r.getDenominator();
-    return Rational(numerator, denominator);
+	int numerator = -(r.getNumerator());
+	int denominator = r.getDenominator();
+	return Rational(numerator, denominator);
 }
 
 //friend
 Rational operator *(const Rational& r1, const Rational& r2){
-    int numerator = r1.numerator * r2.numerator;
-    int denominator = r2.numerator * r2.numerator;
-    return Rational(numerator, denominator);
+	int numerator = r1.numerator * r2.numerator;
+	int denominator = r2.numerator * r2.numerator;
+	return Rational(numerator, denominator);
 }
 
 //friend
 Rational operator /(const Rational& r1, const Rational& r2){
-    int numerator = r1.numerator * r2.denominator;
-    int denominator = r1.denominator * r1.numerator;
-    return Rational(numerator, denominator);
+	int numerator = r1.numerator * r2.denominator;
+	int denominator = r1.denominator * r1.numerator;
+	return Rational(numerator, denominator);
 }
 
 //friend
 bool operator ==(const Rational& r1, const Rational& r2){
-    return (r1.numerator*r2.denominator) == (r1.denominator*r2.numerator);
+	return (r1.numerator*r2.denominator) == (r1.denominator*r2.numerator);
 }
 
 //member
 bool Rational::operator <(const Rational& r2) const{
-    return (numerator*r2.denominator) < (denominator*r2.numerator);
+	return (numerator*r2.denominator) < (denominator*r2.numerator);
 }
 
 //member
 bool Rational::operator <=(const Rational& r2) const{
-    return (numerator*r2.denominator) <= (denominator*r2.numerator);
+	return (numerator*r2.denominator) <= (denominator*r2.numerator);
 }
 
 //member
 bool Rational::operator >(const Rational& r2) const{
-    return (numerator*r2.denominator) > (denominator*r2.numerator);
+	return (numerator*r2.denominator) > (denominator*r2.numerator);
 }
 
 //member
 bool Rational::operator >=(const Rational& r2) const{
-    return (numerator*r2.denominator) >= (denominator*r2.numerator);
+	return (numerator*r2.denominator) >= (denominator*r2.numerator);
 }
 
 //member
 int& Rational::operator [](int index){
-    if(index == 0)
-        return numerator;
-    else if(index == 1)
-        return denominator;
-    else
-        cout<<"Wrong index.\n";
+	if(index == 0)
+		return numerator;
+	else if(index == 1)
+		return denominator;
+	else
+		cout<<"Wrong index.\n";
 }
 
 int main(){
-    return 0;
+	return 0;
 }
